41727f9af8141735c017f31090f891cefff30f94
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-01-31 01:52:57 +0200

    срфт

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
+const dotenv = require("dotenv");
 
-const DB_HOST =
-  "mongodb+srv://Max:11Dmytro11@cluster0.bgjfnff.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
+dotenv.config();
+// const DB_HOST =
+//   "mongodb+srv://Max:11Dmytro11@cluster0.bgjfnff.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => console.log("meowwwwwwwwwwwwwwwwwwww"))
